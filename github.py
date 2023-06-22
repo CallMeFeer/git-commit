0b9d8e6ae2cdb07d0673ba36dcb775d93eeefc74
CallMeFeer <fernandoanghelcastro@gmail.com>
2023-06-22 10:46:59 -0400

    Last test

--- github.py
+++ github.py
@@ -7,21 +7,22 @@ async def init():
 	await asyncio.sleep(1.5)
 	options = int(input('\n---------------------------------------\n1) Create a new repository\n2) Push an existing repository\n'))
 	if options == 1:
-		git_add = input('git add ')
+		git_add = input('\n---------------------------------------\ngit add ')
 		os.system(f'git add {git_add}')
-
 		git_commit = input('git commit -m ')
 		os.system(f'git commit -m {git_commit}')
-
 		os.system('git branch -M main')
-
 		git_remote = input('git remote add origin ')
 		os.system(f'git remote add origin {git_remote}')
-
 		os.system('git push -u origin main')
 
 	if options == 2:
-		print('It works')
+		git_add = input('\n---------------------------------------\ngit add ')
+		os.system(f'git add {git_add}')
+		git_remote = input('git remote add origin ')
+		os.system(f'git remote add origin {git_remote}')
+		os.system('git branch -M main')
+		os.system('git push -u origin main')
 
 
 asyncio.run(init())
