40c88e37c0fb859161ab3d215e386ec099ab17dd
CallMeFeer <fernandoanghelcastro@gmail.com>
2023-06-22 10:28:03 -0400

    My first commit

--- github.py
+++ github.py
@@ -7,17 +7,16 @@ async def init():
 	await asyncio.sleep(1.5)
 	options = int(input('\n---------------------------------------\n1) Create a new repository\n2) Push an existing repository\n'))
 	if options == 1:
-		#git_add = input('git add ')
-		os.system('git add .')
+		git_add = input('git add ')
+		os.system(f'git add {git_add}')
 
 		git_commit = input('git commit -m ')
-		print(git_commit)
-		os.system(f'git add {git_commit}')
+		os.system(f'git commit -m {git_commit}')
 
 		os.system('git branch -M main')
 
 		git_remote = input('git remote add origin ')
-		os.system(f'git add {git_commit}')
+		os.system(f'git remote add origin {git_remote}')
 
 		os.system('git push -u origin main')
 		
